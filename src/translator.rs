6a5bb8d9dcd6f679e60b5421606d33987f9a665d
Benoit Tellier <benoitprm@pm.me>
2022-11-28 14:37:54 +0100

    Changed the way of escaping html characters as there are some problems with the API. It may still cause some problems with languages that don't use the same characters.

--- src/translator.rs
+++ src/translator.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 
 pub fn translate(source: String, dest: String, text: String) -> Result<String, Box<dyn Error>> {
-    let escaped_text = text.replace("/", "[slash]").replace("?", "[question_mark]");
+    let escaped_text = text.replace("/", "{s}").replace("?", "{q}");
     let request = format!("https://lingva.ml/api/v1/{source}/{dest}/{escaped_text}");
 
     let resp = reqwest::blocking::get(request)?.text()?;
@@ -9,7 +9,7 @@ pub fn translate(source: String, dest: String, text: String) -> Result<String, B
     let parsed = json::parse(&resp).unwrap();
     if parsed.has_key("translation") {
         let result = parsed["translation"].dump();
-        return Ok(result[1 .. result.len() - 1].to_string().replace("[slash]", "/").replace("[question_mark]", "?"));
+        return Ok(result[1 .. result.len() - 1].to_string().replace("{s}", "/").replace("{q}", "?"));
     }
 
     Ok(resp)
